e9c509c056ecc6a8325df602e4d8feb72369615d
Alecerzea <85199063+Alecerzea@users.noreply.github.com>
2025-04-18 14:37:37 -0500

    Update user.js

--- user.js
+++ user.js
@@ -31,4 +31,3 @@ user_pref("privacy.sanitize.sanitizeOnShutdown", true);
 user_pref("privacy.trackingprotection.cryptomining.enabled", true);
 user_pref("privacy.trackingprotection.enabled", true);
 user_pref("webgl.disabled", true);
-user_pref("browser.startup.page", 3);
